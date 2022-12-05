edf1f3190c8bf191fcb11fe0e20dd37c9bd6f070
tonytec <tonyexplorer202@gmail.com>
2022-12-05 14:15:05 +0100

    Add 10-divisible_by_2.py file

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -4,4 +4,8 @@ def divisible_by_2(my_list=[]):
     check_list = []
 
     for num in my_list:
-        
+        if num % 2 == 0:
+            check_list.append(True)
+        else:
+            check_list.append(False)
+    return check_list
--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -2,10 +2,10 @@
 
 def max_integer(my_list=[]):
     if len(my_list) == 0:
-        return None
+        return "None"
 
     max = my_list[0]
-    
+
     for num in my_list:
         if num > max:
             max = num
