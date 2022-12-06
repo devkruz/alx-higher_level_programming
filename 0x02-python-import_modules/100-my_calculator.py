395c7d6327165e9bb5af0b86e1a7d89a85335ca9
tonytec <tonyexplorer202@gmail.com>
2022-12-06 19:04:02 +0100

    Fix simple calculator error

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -4,19 +4,15 @@ if __name__ == "__main__":
     from sys import argv, exit
     from calculator_1 import add, sub, mul, div
 
-    arg_len = len(argv)
-
-    if arg_len < 3:
-        print("{} <a> <operator> <b>".format(argv[0]))
+    if len(argv) - 1 != 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
 
-    operator = [["+", add],["-", sub], ["*", mul], ["/", div], ["null"]]
+    ops = {"+": add, "-": sub, "*": mul, "/": div}
+    if argv[2] not in list(ops.keys()):
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
 
-    for op in operator:
-        if op[0] == argv[2]:
-            print("{} {} {} = {}"
-            .format(argv[1], argv[2], argv[3], op[1](argv[1], argv[3])))
-            break;
-        elif op[0] == "null":
-            print("Unknown operator. Available operators: +, -, * and /")
-            exit(1)
+    a = int(argv[1])
+    b = int(argv[3])
+    print("{} {} {} = {}".format(a, argv[2], b, ops[argv[2]](a, b)))
