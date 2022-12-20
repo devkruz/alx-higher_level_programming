0f86915c1703d2c6d769e583c47e874416582a78
tonytec <tonyexplorer202@gmail.com>
2022-12-20 03:17:44 +0100

    Add 6-raise_exception_msg.py file

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,16 +2,24 @@
 
 def best_score(a_dictionary):
 
-    if isinstance(a_dictionary, dict):
-        keys = []
+    if isinstance(a_dictionary, dict) and len(a_dictionary) > 0:
+        max_key = list(a_dictionary.keys())[0]
+        max_value = a_dictionary[max_key]
 
-        for key in a_dictionary.keys():
-            keys.append(key)
-        max = a_dictionary[keys[0]]
-        for key in keys:
-            if a_dictionary[key] > max:
-                max = a_dictionary[key]
+        for key, value in a_dictionary.items():
+            if value > max_value:
+                max_value = value
+                max_key = key
 
-        return max
+        return max_key
     else:
         return "None"
+
+
+
+a_dictionary = {'John': 12, 'Bob': 14, 'Mike': 14, 'Molly': 16, 'Adam': 10}
+best_key = best_score(a_dictionary)
+print("Best score: {}".format(best_key))
+
+best_key = best_score(None)
+print("Best score: {}".format(best_key))
