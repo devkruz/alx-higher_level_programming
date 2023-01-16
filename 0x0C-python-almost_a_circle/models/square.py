04b9d04456ee57e3c32cb7665e026e55d5512c13
tonytec <tonyexplorer202@gmail.com>
2023-01-16 17:52:17 +0100

    Add update methode

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -18,6 +18,61 @@ class Square(Rectangle):
         """ Instance initialization """
         super().__init__(width=size, height=size, x=x, y=y, id=id)
 
+    @property
+    def size(self):
+        """ get/set instance width and height """
+        return self.width
+
+    @size.setter
+    def size(self, size):
+        if not isinstance(size, int):
+            raise TypeError("{} must be an integer".format("width"))
+        if size <= 0:
+            raise ValueError("{} must be > 0".format("width"))
+        self.width = size
+        self.height = size
+
+    def update(self, *args):
+        """ updates the instance attributes
+            args:
+                args[0](int) - id
+                args[1](int) - size
+                args[2](int) - x
+                args[3](int) - y
+         """
+        try:
+            id = args[0]
+            super().__init__(width=self.width, height=self.height, id=id)
+        except IndexError:
+            pass
+        try:
+            size = args[1]
+            if not isinstance(size, int):
+                raise TypeError("{} must be an integer".format("width"))
+            if size <= 0:
+                raise ValueError("{} must be > 0".format("width"))
+            self.size = size
+        except IndexError:
+            pass
+        try:
+            x = args[2]
+            if not isinstance(x, int):
+                raise TypeError("{} must be an integer".format("x"))
+            if x < 0:
+                raise ValueError("{} must be >= 0".format("x"))
+            self.x = x
+        except IndexError:
+            pass
+        try:
+            y = args[3]
+            if not isinstance(y, int):
+                raise TypeError("{} must be an integer".format("y"))
+            if y < 0:
+                raise ValueError("{} must be >= 0".format("y"))
+            self.y = y
+        except IndexError:
+            pass
+
     def __str__(self):
         """ Instance representation """
         return "[Square] ({}) {}/{} - {}"\
