477894e36ca862e17b06238b6c56c03ec6738bc3
tonytec <tonyexplorer202@gmail.com>
2023-01-16 19:02:19 +0100

    Add json string representation to base class

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ Base class defination """
-
+from json import dumps
 
 class Base:
     """
@@ -18,10 +18,23 @@ class Base:
         """ Instance initialization
         args:
             id(int): Instance id
-         """
+        """
 
         if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """
+        JSON string representation of a list dictionary
+        args:
+            list_dictionaries: list dictionary
+
+        """
+        if list_dictionaries is None or\
+        list_dictionaries == []:
+            return []
+        return dumps(list_dictionaries)
