4706514639e565b40f88cd1ea83a1ac3f97e0dd2
tonytec <tonyexplorer202@gmail.com>
2023-03-29 13:08:30 +0100

    Add 2-square.py file

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Square class"""
+
+
+class Square:
+    """Square class defination"""
+
+    def __init__(self, size=0) -> None:
+        """Initialize a new instance
+
+            Args:
+                size: The size of the square instance
+        """
+
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
