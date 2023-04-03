20155d424550e473149182f5dbb2d718ba8194fb
tonytec <tonyexplorer202@gmail.com>
2023-04-03 15:59:55 +0100

    Add 101-nqueens.py file

---
+++ 0x08-python-more_classes/101-nqueens.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import sys
+"""The Nqueen"""
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+    if sys.argv[1].isdigit() is False:
+        print("N must be a number")
+        sys.exit(1)
+    if int(sys.argv[1]) < 4:
+        print("N must be at least 4")
+        sys.exit(1)
