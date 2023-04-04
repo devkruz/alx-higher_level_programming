df3ffc61b40587914b4c400586e8c293c835fef9
tonytec <tonyexplorer202@gmail.com>
2023-04-04 18:29:35 +0100

    Modify 100-magic_string.py file

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
-"""Magic string"""
-
-
 def magic_string():
-    """Magic string"""
     magic_string.called = getattr(magic_string, "called", 0) + 1
-
     return ("BestSchool" + ", BestSchool" * (magic_string.called - 1))
