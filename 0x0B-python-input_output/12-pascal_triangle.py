93cc63b62078c53b9cdad6a8b687000c07735f6c
tonytec <tonyexplorer202@gmail.com>
2023-04-13 19:53:17 +0100

    Add 12-pascal_triangle.py file

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -4,5 +4,14 @@
 
 def pascal_triangle(n):
     """Pascal triangle"""
-    if n < 0:
+    if n <= 0:
         return []
+    pascal = [[1]]
+    for row in range(n - 1):
+        arr_row = []
+        tmp = [0] + pascal[-1] + [0]
+        for index in range(1, len(tmp)):
+            sum = tmp[index - 1] + tmp[index]
+            arr_row.append(sum)
+        pascal.append(arr_row)
+    return pascal
