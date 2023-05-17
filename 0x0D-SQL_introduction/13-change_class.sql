95096684a4b6bb4b0bd608c6fcdef3576f575d07
tonytec <tonyexplorer202@gmail.com>
2023-05-17 05:06:25 +0100

    Fix 15-groups.sql file

--- 0x0D-SQL_introduction/13-change_class.sql
+++ 0x0D-SQL_introduction/13-change_class.sql
@@ -1,3 +1,3 @@
 -- removes all records with a score <= 5 in the table second_table of the database hbtn_0c_0 
 
-DELETE FROM second_table where score <= 5;
+DELETE FROM second_table WHERE score <= 5;
--- 0x0D-SQL_introduction/8-count_89.sql
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -1,3 +1,3 @@
 -- Display the number of records where id == 89
 
-SELECT COUNT(*) FROM first_table where id = 89;
+SELECT COUNT(*) FROM first_table WHERE id = 89;
