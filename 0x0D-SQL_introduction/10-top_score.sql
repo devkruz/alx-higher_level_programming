d6b0fe557c6d5009bd116e6bbfdf868ab75b5dde
tonytec <tonyexplorer202@gmail.com>
2023-05-17 04:52:56 +0100

    Add more sql scripts

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,5 @@
+-- lists all records of the table second_table of the database
+-- Results display both the score and the name (in this order)
+-- Records ordered by score (top first)
+
+SELECT score, name FROM second_table ORDER BY score DESC;
---
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -0,0 +1,3 @@
+-- Display the number of records where id == 89
+
+SELECT COUNT(*) FROM first_table where id = 89;
