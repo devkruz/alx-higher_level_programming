5a326919ec82a07d728465774a579859a7d0c864
tonytec <tonyexplorer202@gmail.com>
2023-06-14 05:47:29 +0100

    Add 13-add.js file

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,12 +1,11 @@
 #!/usr/bin/node
 
-const num = parseInt(process.argv[2]);
+const num = parseInt(process.argv[2]) ? parseInt(process.argv[2]) : 0;
 
 function fact(num)
 {
-  if (num < 0) return;
-  if (num < 2) return 1;
-  return (num * fact(num - 1));
+  if (num <= 0) return 1;
+  return(num * fact(num - 1));
 }
 
 console.log(fact(num));
---
+++ 0x12-javascript-warm_up/13-add.js
@@ -0,0 +1,4 @@
+#!/usr/bin/node
+modules.exports = function add(a, b) {
+  reutn (a + b);
+}
