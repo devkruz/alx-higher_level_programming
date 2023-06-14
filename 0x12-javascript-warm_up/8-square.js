d051e38a677c9cfdba81b86dc0032b2c3a33e5d2
tonytec <tonyexplorer202@gmail.com>
2023-06-14 04:43:29 +0100

    Add 8-square.js file

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+if (parseInt(process.argv[2])) {
+  const size = process.argv[2];
+  for (let i = 0; i < size; i++) {
+    for (let i = 0; i < size; i++) process.stdout.write('X');
+    console.log('');
+  }
+}
