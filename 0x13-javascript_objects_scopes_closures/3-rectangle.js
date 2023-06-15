7edd47859d7f8d12ead3bd161cbd41eb7bdf70dd
tonytec <tonyexplorer202@gmail.com>
2023-06-15 05:31:27 +0100

    Add 3-rectangle.js file

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w <= 0 || h <= 0) {
+
+    } else {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print() {
+    for (let i = 0; i < this.height; i++)
+    {
+      for (let i = 0; i < this.width; i++) process.stdout.write('X');
+      console.log('');
+    }
+  }
+};
