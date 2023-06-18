511c02b768e3399ddc7795be48b475ab996bd59f
tonytec <tonyexplorer202@gmail.com>
2023-06-18 02:03:00 +0100

    Add 1-filter_states.py file

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" lists all states from the database hbtn_0e_0_usa """
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    cursor = db.cursor()
+    cursor.execute("""SELECT * FROM states WHERE name LIKE
+                    BINARY 'N%'ORDER BY states.id""")
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+    cursor.close()
+    db.close()
