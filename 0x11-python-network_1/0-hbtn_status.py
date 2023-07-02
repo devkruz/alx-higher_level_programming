c1307ab4d2dee3b8b23a9905c1709e8c602ec937
tonytec <tonyexplorer202@gmail.com>
2023-07-02 19:25:52 +0100

    Update 0-hbtn_status.py file

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
-# Fetche https://alx-intranet.hbtn.io/status
+""" Fetch https://alx-intranet.hbtn.io/status """
+
 import urllib.request as request
 
 if __name__ == '__main__':
     with request.urlopen('https://alx-intranet.hbtn.io/status') as resp:
         respond = resp.read()
-        print("Body response:\n- type: {}\n- content: {}\n- utf8 content: {}"
-              .format((type(respond)), respond, respond.decode("utf-8")))
+        type = "\n\t- type: {}".format(type(respond))
+        content = "\n\t- content: {}".format(respond)
+        uft = "\n\t- utf8 content: {}".format(respond.decode("utf-8"))
+        msg = "Body response:" + type + content + uft
+        print(msg)
