2ace701858274c617a6bf3b53e1cf9b92cb9db8a
tonytec <tonyexplorer202@gmail.com>
2023-07-03 02:08:53 +0100

    Add 6-post_email.py file

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-""" Fetch https://alx-intranet.hbtn.io/status """
+"""
+Sends a request to the URL and displays the
+value of the variable X-Request-Id in the response header
+ """
 
 import requests
 import sys
